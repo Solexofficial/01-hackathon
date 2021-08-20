c87d1ebe9ac9fabdf9c9c3d41a04ca51bc34b575
Mark Pokrovsky <solexofficial@yandex.ru>
2021-08-21 00:39:51 +0300

    feat: add random pos

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -7,38 +7,36 @@ export class ShapeModule extends Module {
 		this.#shapes = [
 			{
 				shape: "square",
-				width: "100",
-				height: "100",
-				background: "red",
+				width: 100,
+				height: 100,
 			},
 			{
 				shape: "rectangle",
-				width: "200",
-				height: "100",
-				background: "red",
+				width: 100,
+				height: 50,
 			},
 			{
 				shape: "Circle",
-				width: 100,
-				height: 100,
+				width: 50,
+				height: 50,
 				borderRadius: "50px",
 			},
 			{
 				shape: "Oval",
-				width: 200,
-				height: 100,
+				width: 100,
+				height: 50,
 				borderRadius: "100px / 50px",
 			},
 			{
 				shape: "triangle",
-				width: 200,
-				height: 200,
+				width: 100,
+				height: 100,
 				clipPath: "polygon(50% 0%, 0% 100%, 100% 100%)",
 			},
 			{
 				shape: "hexagon",
-				width: 250,
-				height: 250,
+				width: 100,
+				height: 100,
 				clipPath: "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)",
 			},
 		];
@@ -50,18 +48,19 @@ export class ShapeModule extends Module {
 
 	createShape(shape) {
 		const ms = random(2, 7);
+		const randomPos = random(1, 5);
 		const { width, height, borderRadius, clipPath } = shape;
 		const $shape = document.createElement("div");
 		const $timer = document.createElement("span");
-		$shape.style.width = `${width}px`;
-		$shape.style.height = `${height}px`;
+		$shape.style.width = `${width * randomPos}px`;
+		$shape.style.height = `${height * randomPos}px`;
 		$shape.style.background = `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 		$shape.style.display = "flex";
 		$shape.style.justifyContent = "center";
 		$shape.style.alignItems = "center";
 		$shape.style.position = "absolute";
-		$shape.style.right = `${random(0, window.innerWidth - width)}px`;
-		$shape.style.top = `${random(height, window.innerHeight - height)}px`;
+		$shape.style.right = `${random(0, window.innerWidth - width * randomPos)}px`;
+		$shape.style.top = `${random(height, window.innerHeight - height * randomPos)}px`;
 		if (borderRadius) {
 			$shape.style.borderRadius = borderRadius;
 		}
