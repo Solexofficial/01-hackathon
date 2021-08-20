983bb7818e6da4581733ce37f79547d971dfe751
Mark Pokrovsky <solexofficial@yandex.ru>
2021-08-20 17:17:49 +0300

    feat: base module structure and open-close menu

--- src/app.js
+++ src/app.js
@@ -1,2 +1,13 @@
-import './styles.css'
+import { ContextMenu } from "./menu";
+import { BackgroundModule } from "./modules/background.module";
+import { ClicksModule } from "./modules/clicks.module";
+import { ShapeModule } from "./modules/shape.module";
+import "./styles.css";
 
+const menu = new ContextMenu("#menu");
+const background = new BackgroundModule("module", "bg change");
+const clicks = new ClicksModule("module", "click counter");
+const shape = new ShapeModule("module", "create shape");
+menu.add(background);
+menu.add(clicks);
+menu.add(shape);
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,29 @@
-import {Menu} from './core/menu'
+import { Menu } from "./core/menu";
 
 export class ContextMenu extends Menu {
+	constructor(selector) {
+		super(selector);
+		window.addEventListener("contextmenu", e => {
+			e.preventDefault();
+			const { pageX, pageY } = e;
+			console.log(pageX, pageY);
+			this.open();
+		});
+	}
 
+	open() {
+		console.log(this.el);
+		this.el.classList.add("open");
+	}
+
+	close() {
+		this.el.classList.remove("open");
+	}
+
+	add(module) {
+		const $el = document.createElement("li");
+		$el.classList.add("menu-item");
+		$el.textContent = module.text;
+		this.el.appendChild($el);
+	}
 }
