b20a1a7a167ba2e299d036d97210925138089099
Mark Pokrovsky <solexofficial@yandex.ru>
2021-08-20 22:57:28 +0300

    feat: add trigger on menu-items

--- src/menu.js
+++ src/menu.js
@@ -1,28 +1,46 @@
 import { Menu } from "./core/menu";
+import { BackgroundModule } from "./modules/background.module";
+import { ClicksModule } from "./modules/clicks.module";
+import { ShapeModule } from "./modules/shape.module";
 
 export class ContextMenu extends Menu {
+	#modules;
 	constructor(selector) {
 		super(selector);
+		this.#modules = {
+			background: new BackgroundModule("bg", "bg change"),
+			clicks: new ClicksModule("clicker", "click counter"),
+			shape: new ShapeModule("shape", "create shape"),
+		};
+
+		for (const module in this.#modules) {
+			this.add(this.#modules[module]);
+		}
+
 		window.addEventListener("contextmenu", e => {
 			e.preventDefault();
 			const { pageX, pageY } = e;
-			console.log(pageX, pageY);
 			this.open(pageX, pageY);
 		});
+
+		const $menuItems = document.querySelectorAll(".menu-item");
+		$menuItems.forEach(item =>
+			item.addEventListener("click", e => {
+				const data = e.target.dataset.type;
+				for (const module in this.#modules) {
+					if (this.#modules[module].type === data) {
+						this.#modules[module].trigger();
+						this.close();
+					}
+				}
+			})
+		);
 	}
 
 	open(posX, posY) {
 		this.el.classList.add("open");
 		this.el.style.left = `${posX}px`;
 		this.el.style.top = `${posY}px`;
-		// const $menuItems = document.querySelectorAll(".menu-item");
-		// console.log($menuItems);
-		// console.log(this.el);
-		// $menuItems.forEach(item =>
-		// 	item.addEventListener("click", e => {
-		// 		console.log(e.target.dataset.type);
-		// 	})
-		// );
 	}
 
 	close() {
