6e905e101cf3943e49bb42b1b451f6f3ac8fa1cc
anutati <anutati@yahoo.com>
2021-08-20 23:33:30 -0400

    Implemented background-change module

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,20 @@
 import {Module} from '../core/module'
+import {random} from '../utilis'
 
 export class BackgroundModule extends Module {
+    constructor(type, text) {
+        super(type, text);
+    }
 
+    trigger() {
+        this.changeBackgroundColor();
+    }
+
+    changeBackgroundColor() {
+        let a = random(0, 255);
+        let b = random(0, 255);
+        let c = random(0, 255);
+        document.body.style.backgroundColor = 'rgb(' + a + ',' + b + ',' + c + ')';
+
+    }
 }
