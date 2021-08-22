a70116fbe1b06da0343db39cb0879f91ddc8874e
Denis <jujojq@gmail.com>
2021-08-22 19:40:25 +0300

    almost_done

--- src/modules/clicks-module.js
+++ src/modules/clicks-module.js
@@ -3,8 +3,8 @@ import {Module} from '../core/module'
 export class ClicksModule extends Module {
     constructor(type, text) {
         super(type, text);
-        this.clicks = result;
-        this.doubleClicks = result1;
+        this.click = result;
+        this.doubleClick = result1;
 
     }
 
@@ -12,7 +12,7 @@ export class ClicksModule extends Module {
         this.oneTwoClicks();
         setTimeout(() => {
             
-            alert(`Вы совершили одиночных кликов ${this.clicks}, двойных кликов ${this.doubleClicks} за ${setTimeout[1]} секунд`);
+            alert(`Вы совершили одиночных кликов ${this.click}, двойных кликов ${this.doubleClick} за ${setTimeout[1]} секунд`);
         }, 10000);
         let timeIsOver = false;
             if (setTimeout) {
