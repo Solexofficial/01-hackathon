76a6c03843a70cc8642c511a79325621e4555025
Denis <jujojq@gmail.com>
2021-08-22 21:34:19 +0300

    msg

---
+++ src/modules/clicks.module.js
@@ -0,0 +1,45 @@
+export class ClicksModule extends Module {
+
+    constructor(type, text) {
+        super(type, text);
+        this.#ms = 10000;
+        this.click = 0;
+        this.doubleClick = 0;
+
+    }
+
+    trigger() {
+        this.oneTwoClicks();
+        setTimeout(() => {
+
+            alert(`Вы совершили одиночных кликов ${this.click}, двойных кликов ${this.doubleClick} за ${this.#ms/1000} секунд`);
+        }, this.#ms);
+        let timeIsOver = false;
+            if (setTimeout) {
+                timeIsOver = true;
+        }
+        console.log(timeIsOver);
+    };
+
+
+    oneTwoClicks() {
+
+        let click = 0;
+            document.addEventListener('click', function clk() {
+            const result = ++click;
+            console.log('one', result);
+            if (timeIsOver === true) {
+                document.removeEventListener('click', clk);
+            }
+});
+
+        let doubleclick = 0;
+            document.addEventListener('dblclick', function dblclk() {
+                const result1 = ++doubleclick;
+                console.log('double', result1);
+                if (timeIsOver === true) {
+                    document.removeEventListener('dblclick', dblclk);
+                }
+        }); 
+    }
+}
